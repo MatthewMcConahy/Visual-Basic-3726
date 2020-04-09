ad323ae9339e33ac45d478974a443150dea1f805
Matthew McConahy <matthewmcconahy123@gmail.com>
2020-04-08 20:10:57 -0400

    Homework 3 initial upload

---
+++ Homework3_McConahy/Form1.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveFlavorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AddCoffeeFlavorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RemoveCoffeeFlavorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearFlavorsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DisplayFlavorCountToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ContextMenuStrip2 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ContextMenuStrip3 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.FlavorsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AddingFlavorComboBox = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.ContextMenuStrip3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(265, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SaveFlavorToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'SaveFlavorToolStripMenuItem
+        '
+        Me.SaveFlavorToolStripMenuItem.Name = "SaveFlavorToolStripMenuItem"
+        Me.SaveFlavorToolStripMenuItem.Size = New System.Drawing.Size(133, 22)
+        Me.SaveFlavorToolStripMenuItem.Text = "Save Flavor"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(133, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AddCoffeeFlavorToolStripMenuItem, Me.RemoveCoffeeFlavorToolStripMenuItem, Me.ClearFlavorsToolStripMenuItem, Me.DisplayFlavorCountToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'AddCoffeeFlavorToolStripMenuItem
+        '
+        Me.AddCoffeeFlavorToolStripMenuItem.Name = "AddCoffeeFlavorToolStripMenuItem"
+        Me.AddCoffeeFlavorToolStripMenuItem.Size = New System.Drawing.Size(190, 22)
+        Me.AddCoffeeFlavorToolStripMenuItem.Text = "Add Coffee Flavor"
+        '
+        'RemoveCoffeeFlavorToolStripMenuItem
+        '
+        Me.RemoveCoffeeFlavorToolStripMenuItem.Name = "RemoveCoffeeFlavorToolStripMenuItem"
+        Me.RemoveCoffeeFlavorToolStripMenuItem.Size = New System.Drawing.Size(190, 22)
+        Me.RemoveCoffeeFlavorToolStripMenuItem.Text = "Remove Coffee Flavor"
+        '
+        'ClearFlavorsToolStripMenuItem
+        '
+        Me.ClearFlavorsToolStripMenuItem.Name = "ClearFlavorsToolStripMenuItem"
+        Me.ClearFlavorsToolStripMenuItem.Size = New System.Drawing.Size(190, 22)
+        Me.ClearFlavorsToolStripMenuItem.Text = "Clear Flavors"
+        '
+        'DisplayFlavorCountToolStripMenuItem
+        '
+        Me.DisplayFlavorCountToolStripMenuItem.Name = "DisplayFlavorCountToolStripMenuItem"
+        Me.DisplayFlavorCountToolStripMenuItem.Size = New System.Drawing.Size(190, 22)
+        Me.DisplayFlavorCountToolStripMenuItem.Text = "Display Flavor Count"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'ContextMenuStrip2
+        '
+        Me.ContextMenuStrip2.Name = "ContextMenuStrip2"
+        Me.ContextMenuStrip2.Size = New System.Drawing.Size(61, 4)
+        '
+        'ContextMenuStrip3
+        '
+        Me.ContextMenuStrip3.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FlavorsToolStripMenuItem})
+        Me.ContextMenuStrip3.Name = "ContextMenuStrip3"
+        Me.ContextMenuStrip3.Size = New System.Drawing.Size(112, 26)
+        '
+        'FlavorsToolStripMenuItem
+        '
+        Me.FlavorsToolStripMenuItem.Name = "FlavorsToolStripMenuItem"
+        Me.FlavorsToolStripMenuItem.Size = New System.Drawing.Size(111, 22)
+        Me.FlavorsToolStripMenuItem.Text = "Flavors"
+        '
+        'AddingFlavorComboBox
+        '
+        Me.AddingFlavorComboBox.FormattingEnabled = True
+        Me.AddingFlavorComboBox.Location = New System.Drawing.Point(37, 71)
+        Me.AddingFlavorComboBox.Name = "AddingFlavorComboBox"
+        Me.AddingFlavorComboBox.Size = New System.Drawing.Size(121, 21)
+        Me.AddingFlavorComboBox.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Coffee Flavor"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(265, 206)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.AddingFlavorComboBox)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "MainForm"
+        Me.Text = "Betsy's Brews"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ContextMenuStrip3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveFlavorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AddCoffeeFlavorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RemoveCoffeeFlavorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearFlavorsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DisplayFlavorCountToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents ContextMenuStrip2 As ContextMenuStrip
+    Friend WithEvents ContextMenuStrip3 As ContextMenuStrip
+    Friend WithEvents FlavorsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AddingFlavorComboBox As ComboBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Homework3_McConahy/Form1.vb
@@ -0,0 +1,211 @@
+﻿'Project: Homework 3
+'Author: Matthew McConahy  
+'Date: Spring 2020
+'Description: 
+'Being able to read in text files, edit. Display coffee flavors and 
+'Use of MenuItems
+Imports System.IO ' using classes from this namespace
+
+Public Class MainForm '
+    Const FileNameStringCONST As String = "flavors.txt" 'used To store filename
+    Dim OrginalFileSize As Integer ' If this value is different from when the file is loaded, it will save
+    Dim OrginalFileSizeChanges As Integer ' if this value differs from the orginal. The user will be promoted to save 
+
+    Private Sub DisplayFlavorCountToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DisplayFlavorCountToolStripMenuItem.Click
+        ' will display the amount of flavors currently
+        MessageBox.Show("The amount of flavors " +
+                        AddingFlavorComboBox.Items.Count.ToString,
+                        "Amount of Flavors")
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        ' save the file before exiting, if they're changes
+        CloseFile()
+    End Sub
+
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' loads automatically, will check for the file
+        DoesFileExist(FileNameStringCONST)
+    End Sub
+
+    Sub CloseFile()
+        ' will kill the program and check if the file has had any changes to do it
+        Dim ChangeBoolean As Boolean ' will check the statements, if true or false
+        Dim CloseSaveStreamWriter As StreamWriter
+        Dim UserAnswerYesOrNoString As String ' records answer for saving or not
+
+        CloseSaveStreamWriter = New StreamWriter(FileNameStringCONST)
+        ChangeBoolean = OrginalFileSize.CompareTo(OrginalFileSizeChanges) ' check if any changes were ever made
+
+        Try
+            If (ChangeBoolean = True) Then
+                UserAnswerYesOrNoString = MessageBox.Show("Changes have been made to the file, Do you want to save",
+                                "Save File",
+                                MessageBoxButtons.YesNo)
+            Else
+                ' nothing happens files werent changed
+            End If
+
+            If (UserAnswerYesOrNoString = vbYes) Then
+                ' will write out to the file
+                For counter As Integer = 0 To AddingFlavorComboBox.Items.Count - 1
+                    Dim SaveFileLineString As String
+                    SaveFileLineString = AddingFlavorComboBox.Items.Item(counter)
+                    CloseSaveStreamWriter.WriteLine(SaveFileLineString)
+                Next
+            Else
+                ' nothing, since no user picked no
+            End If
+
+
+        Catch ex As IOException
+            MessageBox.Show("File was unable to be saved")
+
+        Finally
+            If CloseSaveStreamWriter IsNot Nothing Then
+                CloseSaveStreamWriter.Close()
+                Close()
+            End If
+        End Try
+
+    End Sub
+
+    Sub SaveFile()
+        ' will save the flavor called on the file 
+        Dim SaveStreamWriter As StreamWriter
+        SaveStreamWriter = New StreamWriter(FileNameStringCONST)
+
+        Try
+            For counter As Integer = 0 To AddingFlavorComboBox.Items.Count - 1
+                Dim SaveFileLineString As String
+                SaveFileLineString = AddingFlavorComboBox.Items.Item(counter)
+                SaveStreamWriter.WriteLine(SaveFileLineString)
+            Next
+        Catch ex As IOException
+            MessageBox.Show("Error Saving the file", "Unable to save the file")
+            ' will kill the stream
+        Finally
+            If SaveStreamWriter IsNot Nothing Then
+                SaveStreamWriter.Close()
+            End If
+        End Try
+    End Sub
+
+    Function DoesFileExist(FileNameStringCONST As String)
+        ' function will check if file exists 
+        ' if the file does not exist, user will be prompted to enter it
+        Dim UserAnswerString As String
+        Dim counter As Integer
+        Dim FileStreamReader As StreamReader
+        Dim FlavorStreamWriter As StreamWriter
+
+        Try
+            If (File.Exists(FileNameStringCONST) = True) Then
+                FileStreamReader = New StreamReader(FileNameStringCONST)
+
+                ' feeds all the strings from the file
+                Do Until FileStreamReader.EndOfStream
+                    Dim LineString As String ' will make lines to bring into file
+                    LineString = FileStreamReader.ReadLine
+                    AddingFlavorComboBox.Items.Add(LineString).ToString()
+                Loop
+                OrginalFileSize = AddingFlavorComboBox.Items.Count
+
+                FileStreamReader.Close()
+            Else
+                ' will ask the user to create the file
+                UserAnswerString = MessageBox.Show("Do you want to create the file",
+                                               "File does not exist",
+                                               MessageBoxButtons.YesNo,
+                                               MessageBoxIcon.Question)
+                ' will create file for user to work with
+            End If
+            If (UserAnswerString = vbYes) Then
+                FlavorStreamWriter = New StreamWriter(FileNameStringCONST, True)
+                FlavorStreamWriter.Close()
+            Else
+                'close application
+            End If
+        Catch ex As IOException
+            MessageBox.Show("Error opening file", "File Error")
+        End Try
+
+
+    End Function
+
+    Sub AddFlavors()
+        ' will append the flavors to list
+        Dim AddingFlavorString As String
+
+        AddingFlavorString = AddingFlavorComboBox.Text
+        If (AddingFlavorString = "") Then
+            MessageBox.Show("You need to enter a flavor name", "Missing flavor name")
+        Else
+            AddingFlavorComboBox.Items.Add(AddingFlavorString)
+            OrginalFileSizeChanges = OrginalFileSize + 1
+        End If
+    End Sub
+
+    Sub RemoveFlavors()
+        'Removing flavor from total
+        Dim RemovingFlavorString As String
+
+        RemovingFlavorString = AddingFlavorComboBox.Text
+
+        If (RemovingFlavorString = "") Then
+            MessageBox.Show("You cannot remove a flavor, if it does not exist", "Missing flavor")
+        Else
+            AddingFlavorComboBox.Items.Remove(RemovingFlavorString)
+            OrginalFileSizeChanges = OrginalFileSize - 1
+        End If
+    End Sub
+
+    Sub ClearingFlavors()
+        'It will clear the list and will ask the user if they wanna clear the list
+        Dim UserChoiceString As String
+        Dim EmptyString As String
+
+        UserChoiceString = MessageBox.Show("Do you want to clear the list?",
+                                               "Confirmation of clearing",
+                                               MessageBoxButtons.YesNo,
+                                               MessageBoxIcon.Question)
+        ' checks the user input
+        If (UserChoiceString = vbYes) Then
+            AddingFlavorComboBox.Items.Clear()
+            AddingFlavorComboBox.Text = ""
+            OrginalFileSizeChanges = 0
+        Else
+            'will do nothing, since we arent clearing the list
+        End If
+
+
+    End Sub
+
+
+
+    Private Sub SaveFlavorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveFlavorToolStripMenuItem.Click
+        'write and save to file
+        SaveFile()
+    End Sub
+
+    Private Sub AddCoffeeFlavorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AddCoffeeFlavorToolStripMenuItem.Click
+        'adding flavors onto list
+        AddFlavors()
+    End Sub
+
+    Private Sub RemoveCoffeeFlavorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RemoveCoffeeFlavorToolStripMenuItem.Click
+        'remove one flavor at time
+        RemoveFlavors()
+    End Sub
+
+    Private Sub ClearFlavorsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearFlavorsToolStripMenuItem.Click
+        ' remove all the flavors
+        ' will ask user before any impact happens
+        ClearingFlavors()
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        MessageBox.Show("Welcome to Bety's Coffee", "Welcome")
+    End Sub
+End Class
